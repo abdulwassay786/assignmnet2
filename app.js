675fdbf76583fe71254e2da9e4769b3abc20a1e4
Abdul Wassay Ansari <abdulwassayansari786@gmail.com>
2021-03-26 17:56:44 +0500

    assignment2

---
+++ app.js
@@ -0,0 +1,28 @@
+// 1- overwrite value while making new object using Object.assign
+
+// let obj = {
+//     name: "Ahmed"
+// }
+
+// let obj1 = {
+//     name: "Abdul Wassay"
+// }
+
+// console.log(obj)
+// let newObj = Object.assign(obj, obj1)
+// console.log(newObj);
+
+
+// 2- nested ternary operator
+
+// let H = 10;
+// let C = 5;
+// let V = 1;
+// let r = 1
+
+// H = (C == 0)           
+//     ? null             
+//     : (V == r)         
+
+//     console.log(H)
+
